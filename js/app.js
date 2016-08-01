0ea744b95d46ee2ce6216f7ca4871c265f0f9cf1
Korirmitchelle <korirmitchelle@gmail.com>
2016-08-01 15:38:43 +0300

    after js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
 $(function () {
-            $('#wond > li').hover(
-                    function () {
-                        var $this = $(this);
-                        $this.stop().animate({
-                            'width': '480px'
-                        }, 500);
-                        $('.heading', $this).stop(true, true).fadeOut();
-                        $('.sectnDes', $this).stop(true, true).slideDown(500);
-                        $('.description', $this).stop(true, true).fadeIn();
-                    }
+    $('#wond > li').hover(
+        function () {
+            var $this = $(this);
+            $this.stop().animate({
+                'width': '480px'
+            }, 500);
+            $('.heading', $this).stop(true, true).fadeOut();
+            $('.sectnDes', $this).stop(true, true).slideDown(500);
+            $('.description', $this).stop(true, true).fadeIn();
+        },
+        function () {
+            var $this = $(this);
+            $this.stop().animate({
+                'width': '115px'
+            }, 1000);
+            $('.heading', $this).stop(true, true).fadeIn();
+            $('.description', $this).stop(true, true).fadeOut(500);
+            $('.sectnDes', $this).stop(true, true).slideUp(700);
+        }
+    );
+});
