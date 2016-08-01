2d77f67ac3f3f0d8153639b9a124460b5033a5af
Korirmitchelle <korirmitchelle@gmail.com>
2016-08-01 15:07:18 +0300

    wond js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-$(document).foundation()
+$(function () {
+            $('#wond > li').hover(
+                    function () {
+                        var $this = $(this);
+                        $this.stop().animate({
+                            'width': '480px'
+                        }, 500);
+                        $('.heading', $this).stop(true, true).fadeOut();
+                        $('.sectnDes', $this).stop(true, true).slideDown(500);
+                        $('.description', $this).stop(true, true).fadeIn();
+                    }
